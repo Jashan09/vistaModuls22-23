188da5425db7f2b312313e9d2a963e36c7a39a7a
daw25 <navjashan.singh@cirvianum.cat>
2022-09-09 20:24:41 +0200

    1r commit --------------------------------------------------------- inline / block absolute / relative width / height

---
+++ index.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/estils.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+
+
+</head>
+<body>
+
+</body>
+</html>
+
+<?php
+?>
+
+
+
+
+    <div class="navbar1">
+    <ul>
+        <i class="fas fa-clock" style="font-size:36px;"></i>
+        <li class="itemPrincipal"><a href="">Institut Cirviànum de Torelló</a></li>
+    </ul>
+    </div>
+
+    <div class="navbar2">
+    <ul>
+
+        <li><a href="">Estudis</a></li>
+        <li><a href="">Secretaria</a></li>
+        <li><a href="">Contacte</a></li>
+    </ul>
+    </div>
+
+
+
+<!--
+
+<div class="divDelsModuls">
+
+    <div class="modul1"></div>
+    <div class="modul2"></div>
+    <div class="modul3"></div>
+    <div class="modul4"></div>
+
+</div>
+
+-->
